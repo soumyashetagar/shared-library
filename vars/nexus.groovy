b01072fbcf2447c0d937e6294de1eaebe72539eb
Maheshwar Reddy <37240803+maheshwar807@users.noreply.github.com>
2019-11-15 18:34:15 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,6 +1,6 @@
 def call(String msg="default"){
   withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-         sh   'curl -F file=@target/myWebApp_Test-${BUILD_NUMBER}.war -u $userId:$pass http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/hexagon-war/myWebApp_test-"${msg}".war'
+         sh   'curl -F file=@target/myWebApp_Test-${BUILD_NUMBER}.war -u $userId:$pass http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/hexagon-war/myWebApp_test-$msg.war'
 			  } 
 			  
        			}
